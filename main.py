241bd1f8ca0189ad4bdd4fdbe43e908ee4af406f
Vitor Hugo Foscarini dos Santos <vitor.santos@promob.com>
2022-09-09 19:06:08 -0300

    update variables

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+from platform import python_branch
 import pyautogui
 import time
 
 pyautogui.alert("Clique em OK para iniciar o processo, lembre-se de não mexer em nada até o processo finalizar")
 pyautogui.PAUSE = 0.5
 
-like = 3 # Numero de vídeos passados até 1 like
-seconds = 5 # Segundos pra deixar rolando o vídeo
-count = 0 # Número de vídeos que deseja assistir
-
+like = 20 # Numero de vídeos passados até 1 like
+seconds = 3 # Segundos pra deixar rolando o vídeo
+count = 10 # Número de vídeos que deseja assistir
+search = '#sneakers'
 
 pyautogui.press('winleft')
 pyautogui.write('chrome')
@@ -17,7 +18,12 @@ pyautogui.press('enter')
 
 time.sleep(5)
 
-pyautogui.moveTo(1061,561)
+pyautogui.moveTo(835,100,duration=1)
+pyautogui.click()
+pyautogui.write(search)
+pyautogui.press('enter')
+
+pyautogui.moveTo(880,365,duration=1)
 pyautogui.click()
 
 while count > 0:
