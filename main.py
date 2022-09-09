06b6bb9230a256cc1a511df423f0aaf5a7aea810
Vitor Hugo Foscarini dos Santos <vitor.santos@promob.com>
2022-09-09 17:07:23 -0300

    finish

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import time
 
 pyautogui.alert("Clique em OK para iniciar o processo, lembre-se de não mexer em nada até o processo finalizar")
 pyautogui.PAUSE = 0.5
-count = 0
+
+like = 3 # Numero de vídeos passados até 1 like
+seconds = 5 # Segundos pra deixar rolando o vídeo
+count = 0 # Número de vídeos que deseja assistir
 
 
 pyautogui.press('winleft')
@@ -14,14 +17,14 @@ pyautogui.press('enter')
 
 time.sleep(5)
 
-pyautogui.moveTo(1061,561,duration=1)
+pyautogui.moveTo(1061,561)
 pyautogui.click()
 
-while count < 5:
-    time.sleep(5)
+while count > 0:
+    time.sleep(seconds)
 
-    # if (count % 2 == 0):
-    #     pyautogui.press('l')
+    if (count % like == 0):
+        pyautogui.press('l')
 
     pyautogui.press('down')
-    count = count + 1
+    count = count - 1
