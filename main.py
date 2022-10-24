41182269ac9fdfcc066146f1151b3606a7f9dcaf
Vitor Hugo Foscarini dos Santos <vitor.santos@promob.com>
2022-10-24 14:34:06 -0300

    Update comments

--- main.py
+++ main.py
@@ -8,16 +8,19 @@ if (pyautogui.confirm('Clique em OK para iniciar o processo, lembre-se de não m
 
 pyautogui.PAUSE = 0.5
 
+# VARIÁVEIS EDITÁVEIS
 
 likemin = 1 # Número mínimo de videos assistidos para deixar like
 likemax = 10 # Número máximo de videos assistidos para deixar like
 secondsmin = 5 # Número mínimo de segundos para deixar rolando o vídeo
 secondsmax = 45 # Número máximo de segundos para deixar rolando vídeo
+videos = 1 # Número de vídeos que deseja assistir
+search = '#sneakers' # Texto a ser procurado no Tiktok para filtrar os vídeos
+
+# FIM VARIÁVEIS EDITÁVEIS
 
 like = random.randrange(likemin, likemax) # Número de vídeos passados até 1 like, número aleatório entre 1 e 10, troca toda vez que é deixado um like - Linha 37
 seconds = random.randrange(secondsmin, secondsmax) # Segundos pra deixar rolando o vídeo, número aleatório entre 5 e 45 segundos, troca toda vez que termina de assistir um vídeo - Linha 34
-videos = 1 # Número de vídeos que deseja assistir
-search = '#sneakers' # Texto a ser procurado no Tiktok para filtrar os vídeos
 
 pyautogui.press('winleft')
 pyautogui.write('chrome')
